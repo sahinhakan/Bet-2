1c420eacee2cb5f390cbcb556808acac2f3da312
Hakan Sahin <hakansahince@gmail.com>
2022-01-18 16:41:13 +0300

    BetToken contract is edited.

--- contracts/BetToken.sol
+++ contracts/BetToken.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract BetToken {
-  constructor() public {
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract BetToken is ERC20{
+  constructor() ERC20('BET Token', 'BET') {
+    _mint(msg.sender, 1000000 * 10 ** 18);
   }
 }
